8c013b1636fc238db0a96545fc333aaf3891dda9
Arthurjv027 <thuthujotave@gmail.com>
2023-10-16 19:00:30 -0300

    taste

---
+++ Prova_Arthur_Silva/src/main/java/com/mycompany/prova_arthur_silva/Prova_Arthur_Silva.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prova_arthur_silva;
+
+/**
+ *
+ * @author aoliveira
+ */
+public class Prova_Arthur_Silva {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
